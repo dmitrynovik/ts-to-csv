8ab4260928ca0d425cba4c0ef1217e7c99aeb7ec
Dmitry Novik <dmitry.novik@gmail.com>
2018-07-16 11:57:11 +1000

    Add project files.

---
+++ Ts2CsvConverter/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Ts2CsvConverter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
